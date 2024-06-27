8c2405ddf7154d869dc4393f134ce19a7954b4b6
Stephen Smith <149971513+ssmith-ghub@users.noreply.github.com>
2024-06-27 19:17:30 -0400

    Add files via upload

---
+++ SqlQuery_SDKnowledgecheck.sql
@@ -0,0 +1,19 @@
+﻿-- Select the Customer Name for the OrderID 10310
+
+SELECT Customers.CustomerName, Orders.CustomerID
+FROM Customers
+INNER JOIN 
+Orders
+ON
+Customers.CustomerID = Orders.CustomerID 
+WHERE Orders.OrderID = 10310;
+
+-- Select the address for the supplier of ProductID 40
+
+SELECT Suppliers.Address, Products.SupplierID
+FROM Suppliers
+INNER Join
+Products
+ON
+Suppliers.SupplierID = Products.SupplierID
+WHERE Products.ProductID = 40;
